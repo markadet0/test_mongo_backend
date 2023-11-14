0ee0937b51aa3ce31376737382603cfc7d060e97
markadet <markadet@gmail.com>
2023-11-14 02:24:13 +0100

    save

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Thing = require('./models/thing');
 
 const app = express();
 
 
 mongoose.connect('mongodb+srv://mkd:ironbus@cluster0.tfpefff.mongodb.net/?retryWrites=true&w=majority',)
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
 app.use(express.json());
@@ -24,10 +22,31 @@ app.use((req, res, next) => {
 
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Objet créé !'
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
     });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
 
 app.use('/api/stuff', (req, res, next) => {
@@ -50,6 +69,12 @@ app.use('/api/stuff', (req, res, next) => {
         },
     ];
     res.status(200).json(stuff);
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
